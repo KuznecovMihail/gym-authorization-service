5e3a3e6f94b524e1a43cf923733a1150475187be
KuznecovMihail <mihailkuznecov918@gmail.com>
2025-06-29 21:14:19 +1000

    fix

--- src/subscription/subscription.controller.ts
+++ src/subscription/subscription.controller.ts
@@ -37,7 +37,7 @@ export class SubscriptionController {
 
   @ApiOperation({ summary: "Получить все абонементы" })
   @ApiResponse({ status: 200, type: [ViewSubscriptionDto] })
-  @Get("/subscription")
+  @Get()
   findAll() {
     return this.subscriptionService.findAll();
   }
